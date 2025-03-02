6b0129f7c12c15cff63e7a77b1f55aaeed6eb1bc
coppea <86509331+coppea@users.noreply.github.com>
2025-03-02 14:31:45 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -32,7 +32,6 @@ PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-autoreboot-zh-cn"
